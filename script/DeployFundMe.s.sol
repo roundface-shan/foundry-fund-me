9b573158a978cdcb0b5caf5d17ccba0cfaa93e56
roundface_shan <lice2567614@gmail.com>
2023-11-17 15:12:20 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   script/DeployFundMe.s.sol
    	modified:   src/FundMe.sol
    	modified:   src/PriceConverter.sol
    	modified:   test/FundMeTest.t.sol

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -5,10 +5,11 @@ pragma solidity ^0.8.18;
 import {Script} from "lib/forge-std/src/Script.sol";
 import {FundMe} from "src/FundMe.sol";
 
-contract DepolyFundMe is Script {
-    function run() external {
+contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        FundMe fundMe = new FundMe();
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
